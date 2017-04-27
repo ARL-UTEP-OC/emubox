fe8164bbdb610adc88488d9aa1fc2917079f820f
RaistlinJ <raistlinj@users.noreply.github.com>
2017-04-26 21:17:25 -0600

    installer now runs correctly from cmd and powershell

--- install.bat
+++ install.bat
@@ -1,13 +1,4 @@
 REM Make sure path to pip is set correctly
 pip install virtualenv
 virtualenv workshop-manager
-workshop-manager\Scripts\activate & ^
-pip install flask & ^
-pip install pyvbox & ^
-pip install gevent & ^
-pip install pypiwin32 & ^
-cd workshop-manager\VirtualBoxSDK-5.1.20-114628\sdk\installer\ & ^
-python vboxapisetup.py install & ^
-cd ..\..\..\ & ^
-copy ..\bin\workshop-manager\workshop-manager.py . & ^
-echo Type: python workshop-manager.py to start EmuBox
+workshop-manager\Scripts\activate & pip install flask & pip install pyvbox & pip install gevent & pip install pypiwin32 & cd workshop-manager\VirtualBoxSDK-5.1.20-114628\sdk\installer\ & python vboxapisetup.py install & cd ..\..\..\ & copy ..\bin\workshop-manager\workshop-manager.py . & echo Type: python workshop-manager.py to start EmuBox
