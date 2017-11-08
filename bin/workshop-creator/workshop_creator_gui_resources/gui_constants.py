b0ab0e126e953671b2a5017bf889eb23a722356c
rrobles2 <30307364+rrobles2@users.noreply.github.com>
2017-11-07 19:03:06 -0700

    Updated Constants and menus

--- bin/workshop-creator/workshop_creator_gui_resources/gui_constants.py
+++ bin/workshop-creator/workshop_creator_gui_resources/gui_constants.py
@@ -4,6 +4,7 @@ import os
 BOX_SPACING = 5
 PADDING = 5
 WORKSHOP_CONFIG_DIRECTORY = os.getcwd() + "/workshop_creator_gui_resources/workshop_configs/"
+WORKSHOP_MATERIAL_DIRECTORY = os.getcwd() + "/workshop_creator_gui_resources/workshop_materials/"
 GUI_MENU_DESCRIPTION_DIRECTORY = os.getcwd() + "/workshop_creator_gui_resources/menuDescription.xml"
 VBOXMANAGE_DIRECTORY = "C:/Program Files/Oracle/VirtualBox/VBoxManage.exe"
 WORKSHOP_CREATOR_DIRECTORY = os.getcwd() + "/workshop-creator.py"
