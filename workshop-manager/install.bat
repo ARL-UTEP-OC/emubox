edf9f0099b31377215ffa2ad26b7a928b8017835
RaistlinJ <raistlinJ@users.noreply.github.com>
2017-11-12 17:54:37 -0800

    Fixed path in installer script

--- workshop-manager/install.bat
+++ workshop-manager/install.bat
@@ -1,5 +1,7 @@
+REM name the container that will be created
+set VENV_NAME=manager-container
+
 REM Make sure path to pip is set correctly
-pip install lxml & pip install gi
 pip install virtualenv
-virtualenv workshop-manager
-workshop-manager\Scripts\activate & pip install flask & pip install pyvbox & pip install gevent & pip install pypiwin32 & cd workshop-manager\VirtualBoxSDK-5.1.20-114628\sdk\installer\ & python vboxapisetup.py install & cd ..\..\..\ & xcopy ..\bin\workshop-manager . /E /Y & echo Type: python instantiator.py to start EmuBox
+virtualenv %VENV_NAME%
+%VENV_NAME%\Scripts\activate & pip install flask & pip install pyvbox & pip install gevent & pip install pypiwin32 & cd bin\VirtualBoxSDK-5.1.20-114628\sdk\installer\ & python vboxapisetup.py install & cd ..\..\..\ & echo Type: python instantiator.py to start EmuBox
---
+++ workshop-manager/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+apt-get install python-pip
+# Make sure path to pip is set correctly
+pip install lxml & pip install gi
+pip install virtualenv
+virtualenv workshop-manager
+source ./workshop-manager/bin/activate
+pip install flask
+pip install pyvbox
+pip install gevent
+VBOX_SDK_PATH=`pwd`/workshop-manager/VirtualBoxSDK-5.1.20-114628/sdk/
+cd bin/VirtualBoxSDK-5.1.20-114628/sdk/installer/
+python vboxapisetup.py install
+cd ../../../
+echo Type: python instantiator.py to start EmuBox
