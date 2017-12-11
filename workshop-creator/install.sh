68f7f62b8e3b296a242e4751d0afc967abd667b0
raistlinJ <raistlinJ@users.noreply.github.com>
2017-12-11 09:49:03 -0700

    Created an install script for the workshop-creator in Linux

--- workshop-creator/install.sh
+++ workshop-creator/install.sh
@@ -7,8 +7,6 @@ virtualenv $VENV_NAME
 source ./$VENV_NAME/bin/activate
 pip install lxml
 pip install gi
-apt-get install python-gobject
-apt-get install libgtk-3-dev
 cd bin
-echo Type: python workshop-creator-gu.py to start the workshop-creator-gui
+echo Type: python3 workshop-creator-gu.py to start the workshop-creator-gui
 
