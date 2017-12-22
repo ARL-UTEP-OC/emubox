1a8f430f7262e56db599e176cbe584dbf221573d
RaistlinJ <raistlinJ@users.noreply.github.com>
2017-12-21 23:07:56 -0800

    Fixed typo in install file

--- workshop-creator/install.bat
+++ workshop-creator/install.bat
@@ -10,8 +10,8 @@ virtualenv "%VENV_NAME%"
 
 IF %PYTHONARCH%==32 (
 echo Processing using a 32-bit python27 installation
-%VENV_NAME%\Scripts\activate & pip install lxml & pip install gi & xcopy python27-32bit-gtk3\* "%VENV_NAME%\%PYTHONPACKAGES_PATH%" /E /Y & cd bin & echo Type: python workshop-creator-gu.py to start the workshop-creator-gui
+%VENV_NAME%\Scripts\activate & pip install lxml & pip install gi & xcopy python27-32bit-gtk3\* "%VENV_NAME%\%PYTHONPACKAGES_PATH%" /E /Y & cd bin & echo Type: python workshop_creator_gui.py to start the workshop-creator-gui
 ) ELSE (
 echo Processing using a 64-bit python27 installation
-%VENV_NAME%\Scripts\activate & pip install lxml & pip install gi & xcopy python27-64bit-gtk3\* "%VENV_NAME%\%PYTHONPACKAGES_PATH%" /E /Y & cd bin & echo Type: python workshop-creator-gu.py to start the workshop-creator-gui
+%VENV_NAME%\Scripts\activate & pip install lxml & pip install gi & xcopy python27-64bit-gtk3\* "%VENV_NAME%\%PYTHONPACKAGES_PATH%" /E /Y & cd bin & echo Type: python workshop_creator_gui.py to start the workshop-creator-gui
 )
