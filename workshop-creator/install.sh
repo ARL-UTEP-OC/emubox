2ec056f981621c083c4ff4fa4d826865b8f0527e
raistlinJ <raistlinJ@users.noreply.github.com>
2018-01-12 21:15:36 -0700

    Added creation of startup script

--- workshop-creator/install.sh
+++ workshop-creator/install.sh
@@ -7,6 +7,19 @@ virtualenv $VENV_NAME
 source ./$VENV_NAME/bin/activate
 pip install lxml
 pip install gi
-cd bin
-echo Type: python3 workshop_creator_gui.py to start the workshop-creator-gui
+
+#Generate the script 
+echo "#!/usr/bin/env bash" > start_gui.sh
+echo "#The name of the container used during installation" >> start_gui.sh
+echo VENV_NAME=creator-container >> start_gui.sh
+echo >> start_gui.sh
+echo "#Activate the container and invoke the gui" >> start_gui.sh
+echo source ./$VENV_NAME/bin/activate >> start_gui.sh
+echo cd bin >> start_gui.sh
+echo python3 workshop_creator_gui.py >> start_gui.sh
+
+chmod 755 start_gui.sh
+echo
+echo
+echo Type: ./start_gui.sh to start the workshop-creator-gui
 
