668e21398410e96ac9f1569f3e68b32af725b333
rrobles2 <30307364+rrobles2@users.noreply.github.com>
2018-02-22 13:00:35 -0700

    Update install_linux.sh

--- workshop-creator/install_linux.sh
+++ workshop-creator/install_linux.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 VENV_NAME=creator-container
 
+sudo apt-get install python-gi
+sudo apt-get install pip
+
 pip install virtualenv
 virtualenv $VENV_NAME
 
 source ./$VENV_NAME/bin/activate
 pip install lxml
-pip install gi
+pip install vext
+pip install vext.gi
 
 #Generate the script 
 echo "#!/usr/bin/env bash" > start_creator.sh
@@ -16,7 +20,9 @@ echo >> start_creator.sh
 echo "#Activate the container and invoke the gui" >> start_creator.sh
 echo source ./$VENV_NAME/bin/activate >> start_creator.sh
 echo cd bin >> start_creator.sh
-echo python3 workshop_creator_gui.py >> start_creator.sh
+echo gsettings set com.canonical.Unity integrated-menus true >> start_creator.sh
+echo python workshop_creator_gui.py >> start_creator.sh
+echo gsettings set com.canonical.Unity integrated-menus false >> start_creator.sh
 
 chmod 755 start_creator.sh
 echo
