936441d0113457e048fd4105d7aba9f89bf97513
dgnajera <dgnajera2@miners.utep.edu>
2018-05-18 10:28:25 -0600

    Updated installer for creator to be able to run manager process.

--- workshop-creator/install_linux.sh
+++ workshop-creator/install_linux.sh
@@ -9,10 +9,22 @@ pip install virtualenv
 virtualenv $VENV_NAME
 
 source ./$VENV_NAME/bin/activate
+#creator dependencies
 pip install pygobject
 pip install lxml
 pip install vext
 pip install vext.gi
+
+#manager dependencies
+pip install flask
+pip install pyvbox
+pip install gevent
+pip install gevent-socketio
+VBOX_INSTALL_PATH=$(which virtualbox)
+VBOX_SDK_PATH=`pwd`/../workshop-manager/bin/VirtualBoxSDK-5.1.20-114628/sdk/
+cd ../workshop-manager/bin/VirtualBoxSDK-5.1.20-114628/sdk/installer/
+python vboxapisetup.py install
+cd ../../../../../workshop-creator/
 
 #Generate the script 
 echo "#!/usr/bin/env bash" > start_creator.sh
@@ -25,6 +37,10 @@ echo cd bin >> start_creator.sh
 echo gsettings set com.canonical.Unity integrated-menus true >> start_creator.sh
 echo python workshop_creator_gui.py >> start_creator.sh
 echo gsettings set com.canonical.Unity integrated-menus false >> start_creator.sh
+echo "#These variables are set based on their values when the install script is executed. Re-set values as needed." >> start_creator.sh
+echo export VBOX_INSTALL_PATH=$VBOX_INSTALL_PATH >> start_creator.sh
+echo export VBOX_SDK_PATH=$VBOX_SDK_PATH >> start_creator.sh
+echo export VBOX_PROGRAM_PATH=$VBOX_PROGRAM_PATH >> start_creator.sh
 
 chmod 755 start_creator.sh
 echo
