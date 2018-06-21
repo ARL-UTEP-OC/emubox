282a3e4cc44976966fa53125aead2d4e656b64c5
raistlinJ <raistlinJ@users.noreply.github.com>
2018-06-21 10:24:03 -0600

    Updated GTK3 and PyGObject build on Windows to 3.24

--- workshop-creator/install_win.bat
+++ workshop-creator/install_win.bat
@@ -3,12 +3,14 @@ REM Edit the following variables according to python installation
 set PYTHONARCH=64
 
 REM ask user for version of python
-set /P version64="Is your python installation 64-bit? (Y/N): "
+set /P version64="Is your python installation Python 2.7.* 64-bit? (Y/N): "
 IF NOT "%version64%"=="Y" IF NOT "%version64%"=="y" (
 set PYTHONARCH=32
 echo *************************************************************************************************
-echo Switching to 32-bit Gtk packages -- If you encounter issues, specify a 64-bit python installation.
+echo Sorry, this version of Python is currently not supported. Please install Python 2.7 64-bit.
 echo *************************************************************************************************
+pause
+exit
 )
 
 REM set directory to place Gtk files
@@ -33,7 +35,7 @@ echo %VENV_NAME%\Scripts\activate ^& cd bin ^& python workshop_creator_gui.py ^&
 echo Type: start_creator.bat to start the workshop-creator-gui
 ) ELSE (
 echo Processing using a 64-bit python27 installation
-%VENV_NAME%\Scripts\activate & pip install lxml & pip install gi & xcopy python27-64bit-gtk3\* "%VENV_NAME%\%PYTHONPACKAGES_PATH%" /E /Y & pip install flask & pip install pyvbox & pip install gevent & pip install gevent-socketio & pip install pypiwin32 & cd ..\workshop-manager\bin\VirtualBoxSDK-5.1.20-114628\sdk\installer\ & python vboxapisetup.py install & cd ..\..\..\..\..\workshop-creator\ & %VENV_NAME%\Scripts\deactivate
+%VENV_NAME%\Scripts\activate & pip install lxml & pip install gi & xcopy python27-64bit-gtk3\* "%VENV_NAME%" /E /Y & pip install flask & pip install pyvbox & pip install gevent & pip install gevent-socketio & pip install pypiwin32 & cd ..\workshop-manager\bin\VirtualBoxSDK-5.1.20-114628\sdk\installer\ & python vboxapisetup.py install & cd ..\..\..\..\..\workshop-creator\ & %VENV_NAME%\Scripts\deactivate
 REM Now create the file that will start the gui
 echo REM the name of the container used during installation > start_creator.bat
 echo set VENV_NAME=creator-container >> start_creator.bat
